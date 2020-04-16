9bb23b4a2b176d31e4f5101b55953f8f096f93fa
Christopher Kyle Horton <christhehorton@gmail.com>
2020-04-15 23:02:50 -0400

    Add more tests for identifiers

---
+++ Xyaneon.Bioinformatics.FASTA.Test/Identifiers/GenInfo/BackboneMolTypeIdentifierTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xyaneon.Bioinformatics.FASTA.Identifiers;
+using Xyaneon.Bioinformatics.FASTA.Identifiers.GenInfo;
+
+namespace Xyaneon.Bioinformatics.FASTA.Test.Identifiers.GenInfo
+{
+    [TestClass]
+    public class BackboneMolTypeIdentifierTest
+    {
+        private const string Code = "bbm";
+        private const int Value = 123;
+
+        [TestMethod]
+        public void Code_ShouldReturnCorrectValue()
+        {
+            Identifier identifier = new BackboneMolTypeIdentifier(Value);
+            Assert.AreEqual(Code, identifier.Code);
+        }
+
+        [TestMethod]
+        public void ToString_ShouldFormatCorrectly()
+        {
+            Identifier identifier = new BackboneMolTypeIdentifier(Value);
+            Assert.AreEqual($"{Code}|{Value}", identifier.ToString());
+        }
+    }
+}
---
+++ Xyaneon.Bioinformatics.FASTA.Test/Identifiers/GenInfo/BackboneSeqIdIdentifierTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xyaneon.Bioinformatics.FASTA.Identifiers;
+using Xyaneon.Bioinformatics.FASTA.Identifiers.GenInfo;
+
+namespace Xyaneon.Bioinformatics.FASTA.Test.Identifiers.GenInfo
+{
+    [TestClass]
+    public class BackboneSeqIdIdentifierTest
+    {
+        private const string Code = "bbs";
+        private const int Value = 123;
+
+        [TestMethod]
+        public void Code_ShouldReturnCorrectValue()
+        {
+            Identifier identifier = new BackboneSeqIdIdentifier(Value);
+            Assert.AreEqual(Code, identifier.Code);
+        }
+
+        [TestMethod]
+        public void ToString_ShouldFormatCorrectly()
+        {
+            Identifier identifier = new BackboneSeqIdIdentifier(Value);
+            Assert.AreEqual($"{Code}|{Value}", identifier.ToString());
+        }
+    }
+}
